5cbd8bc580de5b4baa5ac88072130b340970f9c9
KrotVidit <backendkid@mail.ru>
2023-06-26 22:41:43 +0300

    first commit

---
+++ src/Page/index.php
@@ -0,0 +1,25 @@
+<?php
+$titlePage = "Главная страница";
+require_once("../block/header.php");
+?>
+
+<?php
+require_once("../DataBase/connect.php");
+
+
+$mysql->close();
+
+?>
+
+
+
+
+<?php
+require_once("../block/footer.php");
+?>
+
+
+
+
+
+ 
